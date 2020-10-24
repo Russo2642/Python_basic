3c15d3f7bb9a123784a240391989521dd352d164
Yel-Aman <kismayelaman@gmail.com>
2020-10-24 12:40:46 +0500

    First Commit

---
+++ draft2.py
@@ -0,0 +1,25 @@
+# a = int(input("first number:"))
+# b = int(input("second number:"))
+# c = input("choose operator + or -:")
+
+# if c=="+":
+#     print(a+b)
+# if c=="-":
+#     print(a-b)
+
+
+
+# g= int(input("first number:"))
+# h = int(input("second number:")) 
+
+# for element in range(g,h):
+#     if element%5==0:
+#         print(element)
+
+
+
+n = int(input("your number:"))
+k = 2
+for element in range(2,n):
+    if element%k!=0:
+        print(element)
